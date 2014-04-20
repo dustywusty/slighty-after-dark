3682ea6838f4835a77883c582a318b2fda53f918
clarkda <dusty@clarkda.com>
2014-04-19 22:37:23 -0500

    Initial Commit

---
+++ slightly-after-dark/slightly_after_darkView.h
@@ -0,0 +1,13 @@
+//
+//  slightly_after_darkView.h
+//  slightly-after-dark
+//
+//  Created by dusty on 4/19/14.
+//  Copyright (c) 2014 httpster. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface slightly_after_darkView : ScreenSaverView
+
+@end
---
+++ slightly-after-dark/slightly_after_darkView.m
@@ -0,0 +1,52 @@
+//
+//  slightly_after_darkView.m
+//  slightly-after-dark
+//
+//  Created by dusty on 4/19/14.
+//  Copyright (c) 2014 httpster. All rights reserved.
+//
+
+#import "slightly_after_darkView.h"
+
+@implementation slightly_after_darkView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
